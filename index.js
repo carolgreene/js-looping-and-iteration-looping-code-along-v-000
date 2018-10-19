2b56faa648ba112274a093e75140d954673bc3ac
Carol Greene <carolgreene3@gmail.com>
2018-10-19 22:57:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ wrapGifts(gifts);
 
 function printBadges(names) {
   for (let i = 0; i < names.length; i++) {
-    console.log(`Welcome ${names[i]}! You are employee #${i + 1}`)
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`)
   }
   return names
 }
